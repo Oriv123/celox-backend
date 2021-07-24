27428f4634e7517579b4b9b1a8e8f252708d7ad4
Neomi Goldberg <neomigoldberg@gmail.com>
2021-07-24 10:48:50 +0300

    ori

--- config/index.js
+++ config/index.js
@@ -3,12 +3,13 @@ var config;
 // keys.js - figure out what set of credentials to return
 if (process.env.NODE_ENV === 'production') {
     // we are in production - return the prod set of keys
-    // config = require('./prod')
-    config = require('./dev')
+    config = require('./prod')
+    // config = require('./dev')
 
 } else {
     // we are in development - return the dev keys!!!
-    config = require('./dev')
+    config = require('./prod')
+    // config = require('./dev')
 }
 
 module.exports = config
